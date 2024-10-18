f1aea24da9d034f610e7619fac423963e2f2d944
Joscarmesa <joscarmesa77@gmail.com>
2024-10-18 01:33:04 -0600

    ports app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes import register_routes
+import os
 
 app = Flask(__name__)
 
@@ -7,13 +8,6 @@ app = Flask(__name__)
 register_routes(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-# para implementacion en host
-
-import os
-
-if __name__ == '__main__':
+    # Detecta el puerto del entorno (importante para Google Cloud Run)
     port = int(os.getenv("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
