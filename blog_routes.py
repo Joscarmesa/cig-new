43f835b3908ad1088aa67ff852962bd576e510d7
Joscarmesa <joscarmesa77@gmail.com>
2025-03-31 12:17:29 -0600

    actualizacion blog

--- blog_routes.py
+++ blog_routes.py
@@ -56,4 +56,8 @@ def medicos_familia_prevencion_manejo_enfermedades():
 def cirrosis_diabetes():
     return render_template('blog_content/blog/cirrosis_diabetes.html', canonical_url="https://higadograso.mx/blog/medicos_familia_prevencion_manejo_enfermedades")        
 
+@blog_bp.route('/blog/cirrosis_higadograso')
+def cirrosis_higadograso():
+    return render_template('blog_content/blog/cirrosis_higadograso.html', canonical_url="https://higadograso.mx/blog/cirrosis_higadograso")        
+
 
